2c8eabacc57f57f0b4eeaf889538c97f1a0dc12f
kronss <oleg.chaychenko@gmail.com>
2017-02-27 17:21:50 +0200

    123

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,12 +12,13 @@
 
 #include "libft.h"
 
-size_t		ft_strlen(const char *s)
+int					ft_strlen(const char *s)
 {
-	size_t i;
+	int				i;
 
 	i = 0;
-	while (s[i])
-		i++;
+	if (s != NULL)
+		while (s[i])
+			i++;
 	return (i);
 }
